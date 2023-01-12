53e8b5f10a51ff6ddd84f6a1f9510c0244276f7d
resur <sol.reshur@gmail.com>
2023-01-12 21:22:55 +0900

    data: metadata와 navlinks

---
+++ data/NavLinks.ts
@@ -0,0 +1,7 @@
+const NavLinks: { title: string; link: string }[] = [
+  { title: 'HOME', link: '/' },
+  { title: 'PORTFOLIO', link: '/' },
+  { title: 'TIL', link: '/til' },
+  { title: 'NOTE', link: '/note' },
+];
+export default NavLinks;
---
+++ data/metadata.ts
@@ -0,0 +1,6 @@
+const MetaData = {
+  title: 'HELLO, RESHUR',
+  description: "RESHUR's BLOG",
+  author: 'RESHUR',
+};
+export default MetaData;
