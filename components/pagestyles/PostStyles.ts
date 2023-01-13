0781cdce1408333543fee603253872db895068e7
resur <sol.reshur@gmail.com>
2023-01-14 00:33:43 +0900

    Styles: post에 적용될 style과 til페이지의 style파일

---
+++ components/pagestyles/PostStyles.ts
@@ -0,0 +1,35 @@
+import styled from '@emotion/styled';
+export const frame = styled.section`
+  font-family: 'Chivo Mono', monospace;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 50px;
+  margin: auto;
+  border: 1px solid black;
+  padding: 50px;
+  text-align: center;
+  background-color: #f0b9bc;
+  height: 100vh;
+`;
+export const PostFrame = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: #f0b9bc;
+  margin: 20px 0;
+`;
+export const PostTitle = styled.h1`
+  color: #344564;
+  font-size: 48px;
+  font-weight: 700;
+  margin: 5px 0px;
+  background-color: #f0b9bc;
+  border-bottom: 2px dashed black;
+`;
+export const Date = styled.span`
+  margin-top: 10px;
+  font-size: 14px;
+  color: #344564;
+  background-color: #f0b9bc;
+`;
---
+++ components/pagestyles/TilStyles.ts
@@ -0,0 +1,6 @@
+import styled from '@emotion/styled';
+export const Frame = styled.section`
+  background-color: #e7e1e1;
+  height: 100vh;
+  padding: 50px 70px;
+`;
