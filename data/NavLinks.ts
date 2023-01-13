cb4765b663aa00037ac0eb5ecc44a7e024bf18e9
resur <sol.reshur@gmail.com>
2023-01-14 00:35:23 +0900

    navlink: Note link 삭제

--- data/NavLinks.ts
+++ data/NavLinks.ts
@@ -2,6 +2,5 @@ const NavLinks: { title: string; link: string }[] = [
   { title: 'HOME', link: '/' },
   { title: 'PORTFOLIO', link: '/' },
   { title: 'TIL', link: '/til' },
-  { title: 'NOTE', link: '/note' },
 ];
 export default NavLinks;
